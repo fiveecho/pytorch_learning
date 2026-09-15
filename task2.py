9ec9f91554a3166d2a035c7669465b811edf2af0
Ethan Sia <siayzethan@gmail.com>
2026-09-15 14:45:05 +0800

    Constructed custom Dataset

--- task2.py
+++ task2.py
@@ -13,8 +13,16 @@ import string
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
+lr = 0.005
+epoch_no = 50
+
+min_word_length = 3
+max_word_length = 10
+
+letter_idx = {letter:idx for idx,letter in enumerate(string.ascii_lowercase)}
+
 def generate_point():
-    length = random.randint(3,10)
+    length = random.randint(min_word_length,max_word_length)
 
     start_end = random.choice([0,1])
 
@@ -29,9 +37,20 @@ def generate_point():
 
     return s, start_end
 
+def word_to_onehot(word):
+    word_to_idx = []
+    for char in word:
+        word_to_idx.append(letter_idx[char])
+    encoded_word = F.one_hot(torch.tensor(word_to_idx), num_classes=26).float()
+
+    pad_length = max_word_length - len(word)
+    padding = torch.zeros(pad_length, 26)
+    en_word = torch.cat((encoded_word, padding))
+    return en_word
+
 dataIn = [generate_point() for _ in range(2000)]
 
-class CustomImageDataset(Dataset):
+class CustomDataset(Dataset):
     def __init__(self, data, transform=None, target_transform=None):
         self.labels = [l for _,l in data]
         self.words = [w for w,_ in data]
@@ -50,4 +69,16 @@ class CustomImageDataset(Dataset):
             label = self.target_transform(label)
         return word, label
 
-words = CustomImageDataset(data=dataIn, target_transform=torch.tensor)
+words = CustomDataset(data=dataIn, transform=word_to_onehot,target_transform=torch.tensor)
+
+ratio = sum(l for _, l in dataIn) / len(dataIn)
+print(ratio)
+
+training_words, test_words = random_split(words, [.8,.2])
+train_words, valid_words = random_split(training_words, [.8,.2])
+
+train_DataLoader = DataLoader(train_words, batch_size=64, shuffle=True)
+valid_DataLoader = DataLoader(valid_words, batch_size=64)
+test_DataLoader = DataLoader(test_words, batch_size=64)
+
+
